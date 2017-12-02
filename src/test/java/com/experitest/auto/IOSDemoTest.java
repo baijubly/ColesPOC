8dd92cd89bcf4d5417079576e994ab77ef9adf9b
Johhnnyy <rajotia.dhiraj@gmail.com>
2017-12-03 02:18:53 +0530

    Update IOSDemoTest.java

--- src/test/java/com/experitest/auto/IOSDemoTest.java
+++ src/test/java/com/experitest/auto/IOSDemoTest.java
@@ -21,18 +21,25 @@ public class IOSDemoTest extends BaseTest {
 	public void setUp(@Optional("@os='ios'") String deviceQuery) throws Exception {
 		init(deviceQuery);
 		// Init application / device capabilities
-		//dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
-		//dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
+//		dc.setCapability(MobileCapabilityType.APP, "com.experitest.ExperiBank");
+		dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
 		dc.setCapability("testName", "IOSDemoTest");
+		dc.setCapability("instrumentApp", true);
 		driver = new IOSDriver<>(new URL(getProperty("url",cloudProperties) + "/wd/hub"), dc);
 	}
 
 	@Test
 	public void test() {
-		// Enter the test code
-
+		 /* driver.executeScript("client:client.deviceAction(\"Wake\")");
+		  driver.executeScript("client:client.deviceAction(\"Unlock\")");*/
+		//  driver.context("NATIVE_APP_INSTRUMENTED");
+		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@accessibilityLabel='Username']")));
+		  driver.findElement(By.xpath("//*[@accessibilityLabel='Username']")).sendKeys("company");
+		  driver.findElement(By.xpath("//*[@accessibilityLabel='Password']")).sendKeys("company");
+		  driver.findElement(By.xpath("//*[@accessibilityLabel='loginButton']")).click();
+		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@accessibilityLabel='logoutButton']")));
+		  driver.findElement(By.xpath("//*[@accessibilityLabel='logoutButton']")).click();
 	}
-
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
